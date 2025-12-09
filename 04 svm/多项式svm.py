1a4c5e0890ce5ffd545f1ef9e73c5cb056a08755
YuXin2006 <2188279773@qq.com>
2025-12-09 23:59:08 +0800

    多项式svm

--- 04 svm/多项式svm.py
+++ 04 svm/多项式svm.py
@@ -1,3 +1,26 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from sklearn import svm
+data = np.array([
+    [0.1, 0.7],
+    [0.3, 0.6],
+    [0.4, 0.1],
+    [0.5, 0.4],
+    [0.8, 0.04],
+    [0.42, 0.6],
+    [0.9, 0.4],
+    [0.6, 0.5],
+    [0.7, 0.2],
+    [0.7, 0.67],
+    [0.27, 0.8],
+    [0.5, 0.72]
+])# 建立数据集
+label = [1] * 6 + [0] * 6 #前六个数据的label为1后六个为0
+x_min, x_max = data[:, 0].min() - 0.2, data[:, 0].max() + 0.2
+y_min, y_max = data[:, 1].min() - 0.2, data[:, 1].max() + 0.2
+xx, yy = np.meshgrid(np.arange(x_min, x_max, 0.002),
+                     np.arange(y_min, y_max, 0.002))
+
 plt.figure(figsize=(16, 15))
 
 for i, degree in enumerate([1, 3, 5, 7, 9, 12]):  # 多项式次数选择了1,3,5,7,9,12
